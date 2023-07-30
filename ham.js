c76bbfa11e83802881a6d47bf7f9725466b00a92
Sphinx010 <paultreaker@gmail.com>
2023-07-30 15:24:47 +0100

    added side nav

--- ham.js
+++ ham.js
@@ -1,11 +1,15 @@
 //togle function with the css active class for state handling
 const checkBox = document.getElementById('checkbox');
-const xEffect = g=document.querySelector('.ham');
+const xEffect = document.querySelector('.ham');
+const navSpace = document.querySelector('.nav');
 
 checkBox.addEventListener('change', function() {
     if(this.checked){
         xEffect.classList.add('active');
+        navSpace.classList.add('active');
     }else{
         xEffect.classList.remove('active');
+        navSpace.classList.remove('active');
     }
+    
 });
